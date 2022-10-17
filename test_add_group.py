3e977dd32ffd1ddb06a8925f055480327f134965
elena <prokopyeva.helena@yandex.ru>
2022-10-17 21:54:10 +0300

    Задание 1

---
+++ test_add_group.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+from selenium.webdriver.firefox.options import Options
+
+options = Options()
+options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+
+class UntitledTestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox(executable_path=r'C:\windows\system32\geckodriver.exe', options=options)
+        self.driver.implicitly_wait(30)
+
+
+    def test_untitled_test_case(self):
+        driver = self.driver
+        driver.get("http://localhost/addressbook/")
+        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_xpath("//input[@value='Login']").click()
+        driver.find_element_by_link_text("groups").click()
+        driver.find_element_by_name("new").click()
+        driver.find_element_by_name("group_name").click()
+        driver.find_element_by_name("group_name").send_keys("test4")
+        driver.find_element_by_name("group_header").click()
+        driver.find_element_by_name("group_header").clear()
+        driver.find_element_by_name("group_header").send_keys("test4")
+        driver.find_element_by_name("group_footer").click()
+        driver.find_element_by_name("group_footer").clear()
+        driver.find_element_by_name("group_footer").send_keys("test4")
+        driver.find_element_by_name("submit").click()
+        driver.find_element_by_link_text("groups").click()
+        driver.find_element_by_link_text("Logout").click()
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
