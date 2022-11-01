423da9d4ea704ac649df6b2c1adb5dedae8ca628
elena <prokopyeva.helena@yandex.ru>
2022-11-01 16:30:27 +0300

    Урок 2, задание 1 (исправлено)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import pytest
-from group import Group
-from application import Application
+from model.group import Group
+from fixture.application import Application
+
 
 @pytest.fixture
 def app(request):
@@ -9,14 +10,15 @@ def app(request):
     request.addfinalizer(fixture.destroy)
     return fixture
 
+
 def test_add_group(app):
-    app.login( username="admin", password="secret")
+    app.login(username="admin", password="secret")
     app.create_group(Group(name="test4", header="test4", footer="test4"))
     app.logout()
+
 
 def test_add_empty_group(app):
     app.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
     app.logout()
-
 
